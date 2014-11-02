2855422b13039b380a11beab2a62f3f66e467ba8
Sergey Plis <s.m.plis@gmail.com>
2014-11-02 05:35:48 -0700

    working (but may be still unsafe) nested multiprocessing solution

--- g2g1runner.py
+++ g2g1runner.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append('./tools/')
 import traversal, bfutils
-from multiprocessing import Pool,Process, Queue, cpu_count
+from multiprocessing import Pool,Process, Queue, cpu_count, current_process
 import functools
 import zickle as zkl
 import time, socket
@@ -15,7 +15,7 @@ else:
     # of cores minus 7% for breathing room
     PNUM=cpu_count()-int(0.07*cpu_count())
     PNUM=max((1,PNUM/INPNUM))
-print 'processes: ',PNUM
+print 'processes: ',PNUM, INPNUM
 repeats = 100
 
 def wrapper(fold, n=10, k=10):
@@ -39,17 +39,19 @@ def killall(l):
     for e in l: 
         e.join(timeout=0.001)
         if not e.is_alive():
-            print 'first result'            
+            #print 'first result'            
             for p in l:
                 if p != e:
-                    print 'terminating ', p.name
+                    #print 'terminating ', p.name
                     p.terminate()
                     p.join()
             return True
     return False
 
-def fan_wrapper():
+def fan_wrapper(fold,n=10,k=10):
     scipy.random.seed()
+    curr_proc=current_process() 
+    curr_proc.daemon=False
     output = Queue()
     while True:
         try:
@@ -57,17 +59,17 @@ def fan_wrapper():
             g2 = traversal.increment_u(g,g)
             def inside_wrapper():
                 scipy.random.seed()
-                print ': ',traversal.density(g),':',
+                print fold,': ',traversal.density(g),':',
                 startTime = int(round(time.time() * 1000))
                 s = traversal.eqc(g2, capsize=1000)
                 endTime = int(round(time.time() * 1000))
                 print len(s)
                 output.put({'gt':g,'eq':s,'ms':endTime-startTime})
-                pl = [Process(target=inside_wrapper) for x in range(2)]
-                for e in pl: e.start()
-                while True:
-                    if killall(pl): break
-                r = output.get()           
+            pl = [Process(target=inside_wrapper) for x in range(INPNUM)]
+            for e in pl: e.start()
+            while True:
+                if killall(pl): break
+            r = output.get()           
         except MemoryError:
             print 'memory error... retrying'
             continue
@@ -76,12 +78,12 @@ def fan_wrapper():
 
 
 #for nodes in [10, 15, 20, 30, 60]:
-for nodes in [6]:
+for nodes in [8]:
     z = {}
-    pool=Pool(processes=1)
-    for dens in [0.2, 0.23, 0.28, 0.32, 0.35]:
+    pool=Pool(processes=PNUM)
+    for dens in [0.2, 0.23, 0.28, 0.32, 0.35, 0.45, 0.55, 0.65]:
         e = bfutils.dens2edgenum(dens, n=nodes)
-        eqclasses = pool.map(functools.partial(wrapper, n=nodes, k=e), 
+        eqclasses = pool.map(functools.partial(fan_wrapper, n=nodes, k=e), 
                              range(repeats))
         z[dens] = eqclasses
     pool.close()
