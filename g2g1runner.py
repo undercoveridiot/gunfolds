3994467e57f3c7f7baff67a35931090242f59225
Sergey Plis <s.m.plis@gmail.com>
2014-11-02 04:59:22 -0700

    nested multiprocessing to implement random order in which edges and nodes are traversed. the fastest wins

--- g2g1runner.py
+++ g2g1runner.py
@@ -7,12 +7,14 @@ import zickle as zkl
 import time, socket
 import scipy
 
+INPNUM = 5 # number of randomized starts per graph
 if socket.gethostname().split('.')[0] == 'leibnitz':
     PNUM=45
 else:
     # Setting the number  of parallel running processes  to the number
     # of cores minus 7% for breathing room
-    PNUM=cpu_count()-int(0.07*cpu_count()) 
+    PNUM=cpu_count()-int(0.07*cpu_count())
+    PNUM=max((1,PNUM/INPNUM))
 print 'processes: ',PNUM
 repeats = 100
 
@@ -33,16 +35,6 @@ def wrapper(fold, n=10, k=10):
         break
     return {'gt':g,'eq':s,'ms':endTime-startTime}
 
-def wrapper2(g):
-    scipy.random.seed()
-    g2 = traversal.increment_u(g,g)
-    print ': ',traversal.density(g),':',
-    startTime = int(round(time.time() * 1000))
-    s = traversal.eqc(g2, capsize=10000)
-    endTime = int(round(time.time() * 1000))
-    print len(s)
-    output.put({'gt':g,'eq':s,'ms':endTime-startTime})
-
 def killall(l):
     for e in l: 
         e.join(timeout=0.001)
@@ -55,20 +47,38 @@ def killall(l):
                     p.join()
             return True
     return False
-    
-output = Queue()
-g = bfutils.ringmore(6,12)
-pl = [Process(target=wrapper2, args=(g,)) for x in range(5)]
-for e in pl: e.start()
-while True:
-    if killall(pl): break
-print output.get()['eq']
-stop
+
+def fan_wrapper():
+    scipy.random.seed()
+    output = Queue()
+    while True:
+        try:
+            g = bfutils.ringmore(n,k)
+            g2 = traversal.increment_u(g,g)
+            def inside_wrapper():
+                scipy.random.seed()
+                print ': ',traversal.density(g),':',
+                startTime = int(round(time.time() * 1000))
+                s = traversal.eqc(g2, capsize=1000)
+                endTime = int(round(time.time() * 1000))
+                print len(s)
+                output.put({'gt':g,'eq':s,'ms':endTime-startTime})
+                pl = [Process(target=inside_wrapper) for x in range(2)]
+                for e in pl: e.start()
+                while True:
+                    if killall(pl): break
+                r = output.get()           
+        except MemoryError:
+            print 'memory error... retrying'
+            continue
+        break
+    return r
+
 
 #for nodes in [10, 15, 20, 30, 60]:
 for nodes in [6]:
     z = {}
-    pool=Pool(processes=PNUM)
+    pool=Pool(processes=1)
     for dens in [0.2, 0.23, 0.28, 0.32, 0.35]:
         e = bfutils.dens2edgenum(dens, n=nodes)
         eqclasses = pool.map(functools.partial(wrapper, n=nodes, k=e), 
