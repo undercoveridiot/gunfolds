c715cce78a1483093543ef3eaa202e8a10837c9e
Sergey Plis <s.m.plis@gmail.com>
2015-02-03 10:53:43 -0700

    added prune function internally, changed conditional independence test. not yet working though

--- tools/pc.py
+++ tools/pc.py
@@ -8,50 +8,47 @@ import graphkit as gk
 import zickle as zkl
 import numpy as np
 import itertools as iter
-
 import statsmodels.api as sm
-
 
 def dpc(data):
     n = data.shape[0]
     data = np.asarray(np.r_[data[:,:-1],data[:,1:]])
 
 
-    def not_cindependent(y,x, condset=[], pval=0.05):
+    def cindependent(y,x, condset=[], pval=0.05):
         yd = data[n+int(y)-1,:].T
         if condset: condset=map(lambda k: int(k)-1,condset)
         X  = data[[int(x)-1]+condset,:].T
         X  = sm.add_constant(X)
         est  = sm.OLS(yd,X).fit()
-        print est.pvalues
         return est.pvalues[0] > pval
+
+    def prune(elist, mask, g):
+        for e in mask:
+            g[e[0]][e[1]].remove((0,1))
+            elist.remove(e)
+        to_remove = []
+        for v in g:
+            g[v] = {w for w in g[v] if g[v]}
 
     g  = gk.superclique(n)
     gtr= bfu.gtranspose(g)
     el = gk.edgelist(g)
-    
+
     counter = 0
     to_remove = []
     while counter <= n-2:
+        print el
         for e in el:
-            ind = True
+            ind = False
             for S in [j for j in iter.combinations(
                     [k for k in gtr[e[1]] if e[0] != k],counter)]:
-                if not_cindependent(e[1], e[0], condset=list(S)):
-                    ind = False
+                if cindependent(e[1], e[0], condset=list(S)):
+                    ind = True
                     break
-            if not ind:
+            if ind:
                 to_remove.append(e)
-                el.remove(e)
+                gtr[e[1]].pop(e[0],None)
         counter += 1
-        
-    for e in to_remove:
-        g[e[0]][e[1]].remove((0,1))
-    to_remove = []
-    for v in g:
-        for w in g[v]:
-            if not g[v][w]:
-                to_remove.append((v,w))
-    for v in to_remove:
-        g[v[0]].pop(v[1], None)
+        prune(el, to_remove, g)
     return g
