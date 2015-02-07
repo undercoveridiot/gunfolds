62b4eb5e1c856edd5d12a09779eec9a505f95b7e
Sergey Plis <s.m.plis@gmail.com>
2015-02-07 07:56:54 -0700

    for command line work

---
+++ init.py
@@ -0,0 +1,11 @@
+import sys,os
+TOOLSPATH='~/soft/src/dev/craft/gunfolds/tools/'
+sys.path.append(os.path.expanduser(TOOLSPATH))
+
+import bfutils as bfu
+import graphkit as gk
+import traversal as trv
+import linear_model as lm
+import numpy as np
+import pc
+
