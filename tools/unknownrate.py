b8bac98977fcaf78102a4ed92d598ab445e4c10d
Sergey Plis <s.m.plis@gmail.com>
2015-03-18 12:02:17 -0600

    *** empty log message ***

--- tools/unknownrate.py
+++ tools/unknownrate.py
@@ -86,6 +86,38 @@ def add2set_(ds, H, cp):
     s = set()
     ss = set()
 
+    for gnum in ds:
+        g = bfu.num2CG(gnum, n)
+        glist = []
+        elist = []
+        for e in ds[gnum]:
+            if not e[1] in g[e[0]]:
+                gk.addanedge(g,e)
+                num = bfu.g2num(g)
+                conflict = False
+                for c in cp:
+                   if num & c == c:
+                       conflict = True
+                       break
+
+                if not conflict and not num in s:
+                    if not bfu.call_u_conflicts(g, H):
+                        glist.append(num)
+                        elist.append(e)
+                        s.add(num)
+                        if bfu.call_u_equals(g, H): ss.add(num)
+                gk.delanedge(g,e)
+        for gn in glist: dsr[gn] = elist
+
+    return dsr, ss
+
+def add2set__(ds, H, cp):
+    n = len(H)
+
+    dsr = {}
+    s = set()
+    ss = set()
+
     #Hnum = bfu.ug2num(H)
 
     for gnum in ds:
@@ -105,10 +137,12 @@ def add2set_(ds, H, cp):
                 if not conflict and not num in s:
                     #if not num in s:
                     au = bfu.call_undersamples(g)
+                    #if not bfu.call_u_conflicts(g, H):
                     if not gk.checkconflict(H, g, au=au):
                         glist.append(num)
                         elist.append(e)
                         s.add(num)
+                        #if bfu.call_u_equals(g, H): ss.add(num)
                         if gk.checkequality(H, g, au=au): ss.add(num)
                 gk.delanedge(g,e)
         for gn in glist: dsr[gn] = elist
