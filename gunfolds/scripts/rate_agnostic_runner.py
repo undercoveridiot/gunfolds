2a8b12b7a44dc20741d05bcd6b9b073f8a4c1a5c
Sergey Plis <s.m.plis@gmail.com>
2015-09-27 22:42:37 -0600

    scripting changes

--- gunfolds/scripts/rate_agnostic_runner.py
+++ gunfolds/scripts/rate_agnostic_runner.py
@@ -4,23 +4,23 @@ import gunfolds.tools.graphkit as gk
 from gunfolds.tools import traversal
 import gunfolds.tools.unknownrate as ur
 import gunfolds.tools.zickle as zkl
-from multiprocessing import Pool, Process, Queue, current_process, active_children
+from multiprocessing import Pool, Process, Queue
 import functools
-import time
+import time, sys, os
 import socket
 import scipy
 import timeout_decorator
 from timeout_decorator import TimeoutError
 
-TIMEOUT=30 # seconds timeout
+TIMEOUT=24*3600 # seconds timeout
 KEY = 'ral_async'
 UMAX = 4
 INPNUM = 1  # number of randomized starts per graph
 CAPSIZE = 1000  # stop traversing after growing equivalence class tothis size
 REPEATS = 100
-PNUM = 70 #get_process_count(INPNUM)
+PNUM = get_process_count(INPNUM)
 
-@timeout_decorator.timeout(TIMEOUT, use_signals=False)
+@timeout_decorator.timeout(TIMEOUT)#, use_signals=False)
 def ra_caller(g2, fold):
     startTime = int(round(time.time() * 1000))
     s = ur.liteqclass(g2, verbose=False, capsize=CAPSIZE)
@@ -63,7 +63,7 @@ if __name__ == '__main__':
                  7: [0.2, .25, .3],
                  8: [0.15, 0.2, 0.25, 0.3],
                  9: [.2],
-                 10: [.2],
+                 10: [.15, 0.2, 0.25, 0.3],
                  15: [0.2],
                  20: [0.1],  # 0.15, 0.2, 0.25, 0.3],
                  25: [0.1],
@@ -73,24 +73,18 @@ if __name__ == '__main__':
                  50: [0.05, 0.1],
                  60: [0.05, 0.1]}
 
-    for nodes in [5]:
+    for nodes in [8]:
         z = {}
         pool=Pool(processes=PNUM)
         for dens in densities[nodes]:
             print "{:2}: {:8} : {:10}  {:10}".format('id', 'density', 'eq class', 'time')
             e = bfutils.dens2edgenum(dens, n=nodes)
-
-            #multiprocess([[i, nodes, e] for i in range(REPEATS)], PNUM)
-
-            eqclasses = []
-            for x in pool.imap(functools.partial(ra_wrapper, n=nodes, k=e),
-                               range(REPEATS)):
-                eqclasses.append(x)
-                z[dens] = eqclasses
-                zkl.save(z[dens],
-                         socket.gethostname().split('.')[0]+\
-                         '_nodes_'+str(nodes)+'_density_'+\
-                         str(dens)+'_'+KEY+'_.zkl')
+            eqclasses =  pool.map(functools.partial(ra_wrapper, n=nodes, k=e),range(REPEATS))
+            z[dens] = eqclasses
+            zkl.save(z[dens],
+                     socket.gethostname().split('.')[0]+\
+                     '_nodes_'+str(nodes)+'_density_'+\
+                     str(dens)+'_'+KEY+'_.zkl')
 
             print ''
             print '----'
