59012fd9895e45c846316c606eac73e1ad4f1d90
Sergey Plis <s.m.plis@gmail.com>
2016-05-17 15:05:20 -0600

    added a plotting function that takes care of layout and formatting

--- tools/gtool.py
+++ tools/gtool.py
@@ -14,6 +14,15 @@ def lg2gt(g):
             edges[(v,w)] = gr.add_edge(verts[v], verts[w])
     return gr
 
+def plotg(g):
+    gg = lg2gt(g)
+    pos = gt.all.sfdp_layout(gg)
+    gt.all.graph_draw(gg, pos,
+               vertex_text=gg.vertex_properties['label'],
+               vertex_font_size=32,
+               edge_pen_width=5,
+               vertex_fill_color=[0.62109375,  0.875     ,  0.23828125, 1])
+
 # from graph_tool.all import sfdp_layout, graph_draw
 # gg = gr
 # pos = sfdp_layout(gr)
