5e1386e26bb970c0cb1274c2eebf9cb951d30a03
Sergey Plis <s.m.plis@gmail.com>
2017-02-26 21:04:13 -0700

    fixed incorrect linebreak

--- gunfolds/tools/traversal.py
+++ gunfolds/tools/traversal.py
@@ -844,8 +844,7 @@ def density(g):
 
 
 def udensity(g):
-    return (len(gk.edgelist(g))+len(gk.bedgelist(g))/2)/
-    np.double(len(g)**2 + len(g)*(len(g)-1)/2)
+    return (len(gk.edgelist(g))+len(gk.bedgelist(g))/2) / np.double(len(g)**2 + len(g)*(len(g)-1)/2)
 
 
 def esig(l, n):
